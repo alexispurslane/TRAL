4a9f12464b872ea32615d98c16d0de0eb2dc886e
christopherdumas <christopherdumas@me.com>
2015-08-08 19:29:12 -0700

    Removed mutability in fsm

--- tral.rkt
+++ tral.rkt
@@ -1,3 +1,24 @@
 #lang racket
-(define global (hash))
-(define config (hash))
+(define (make-finite-state-machine lst)
+  (define (hash-set-in ht ks v)
+    (cond [(not (list? ks)) (error "ks not a list")]
+          [(empty? (cdr ks)) (hash-set ht (car ks) v)]
+          [else
+           (hash-set ht
+                     (car ks)
+                     (hash-set-in (hash-ref ht (car ks))
+                                  (cdr ks)
+                                  v))]))
+  (foldl (lambda (x fsm)
+           (define-values (old event state action) (values (first x)
+                                                           (second x)
+                                                           (third x)
+                                                           (fourth x)))
+           (define fsm-rev (if (hash-ref fsm old #f)
+                               fsm
+                               (hash-set fsm old (hash))))
+           (displayln fsm-rev)
+           (hash-set-in fsm-rev `(,old ,event)
+                        (hash "state" state
+                              "action" (action event state))))
+         (hash) lst))
