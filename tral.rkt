4e51cac6b34dadc8cfcab1f45733202788d418de
christopherdumas <christopherdumas@me.com>
2015-08-10 13:06:37 -0700

    Added ability to add inventory commands. Also built the scaffolding for runnning inventory commands.

--- tral.rkt
+++ tral.rkt
@@ -9,17 +9,22 @@
          run-inventory-action
          parse
          repl
-         room)
-(define (make-fsm lst)
-  (define (hash-set-in ht ks v)
-    (cond [(not (list? ks)) (error "ks not a list")]
-          [(empty? (cdr ks)) (hash-set ht (car ks) v)]
-          [else
-           (hash-set ht
-                     (car ks)
-                     (hash-set-in (hash-ref ht (car ks))
-                                  (cdr ks)
-                                  v))]))
+         room
+	 add-inventory-action)
+
+(define actions (hash))
+
+(define (hash-set-in ht ks v [default-value (hash)])
+  (cond [(not (list? ks)) (error "ks not a list")]
+	[(empty? (cdr ks)) (hash-set ht (car ks) v)]
+	[else
+	 (hash-set ht
+		   (car ks)
+		   (hash-set-in (hash-ref ht (car ks) default-value)
+				(cdr ks)
+				v))]))
+
+(define (make-fsm lst) 
   (foldl (lambda (x fsm)
            (define-values (old event state action) (values (first x)
                                                            (second x)
@@ -32,7 +37,6 @@
                         (hash "state" state
                               "action" (action event state))))
          (hash) lst))
-
 (define (run fsm command state)
   (define s (hash-ref (hash-ref fsm state) command))
   ((hash-ref s "action"))
@@ -78,25 +82,32 @@
                        "throw" "drop"
                        "i" "inventory"))
 
-(define (run-inventory-action c)
+(define (run-inventory-action c state object-hash)
   (cond
-    [(= (length c) 2) ; VERB NOUN, i.e. GET LAMP
-     (define verb (first c))
-     (define noun (second c))
-     verb]
-    [(= (length c) 3) ; VERB NOUN NOUN, i.e. SWITCH LAMP ON
-     (define verb (first c))
-     (define noun (second c))
-     (define noun2 (third c))
-     verb]
-    [(= (length c) 4) ; VERB NOUN PRONOUN NOUN, i.e. PUT LAMP ON STOOL
-     (define verb (first c))
-     (define noun (second c))
-     (define prep (third c))
-     (define noun2 (fourth c))
-     verb]))
+   [(= (length c) 2) ; VERB NOUN, i.e. GET LAMP
+    (define verb (first c))
+    (define noun (second c))
+    verb]
+   [(= (length c) 3) ; VERB NOUN NOUN, i.e. SWITCH LAMP ON
+    (define verb (first c))
+    (define noun (second c))
+    (define noun2 (third c))
+    verb]
+   [(= (length c) 4) ; VERB NOUN PREPOSITION NOUN, i.e. PUT LAMP ON STOOL
+    (define verb (first c))
+    (define noun (second c))
+    (define prep (third c))
+    (define noun2 (fourth c))
+    verb]))
 
-(define (parse fsm current-state str)
+(define (add-inventory-action action f [h (hash)])
+  (hash-set-in h action (apply f action)))
+
+(define (add-inventory-actions action-list [h (hash)])
+  (foldl (lambda (a ht)
+	   (add-inventory-action (first a) (second a) ht)) h action-list))
+
+(define (parse fsm current-state str object-hash)
   (define wic #f)
   (define input (string-split str))
   (define command (if (and (not (member (first input) commands))
@@ -109,14 +120,14 @@
                                       (first input))))
                       (first input)))
   (cond
-    [(and (not (equal? command "quit")) (not wic))
-     (run fsm command current-state)]
-    [(and (not (equal? command "quit")) wic)
-     (run-inventory-action input)]
-    [(equal? command "quit") command]))
+   [(and (not (equal? command "quit")) (not wic))
+    (run fsm command current-state)]
+   [(and (not (equal? command "quit")) wic)
+    (run-inventory-action input current-state object-hash)]
+   [(equal? command "quit") command]))
 
-(define (repl fsm [state "start"] [icommand "begin"])
-  (define res (parse fsm state (or icommand (read-line))))
+(define (repl fsm [state "start"] [icommand "begin"] [object-hash (hash)])
+  (define res (parse fsm state (or icommand (read-line)) object-hash))
   (if (not (equal? res "quit"))
       (repl fsm res #f)
       (displayln "Bye!")))
