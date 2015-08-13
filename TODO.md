01d077d6668e1dc5f87114b5249f8c6c1cd33f99
christopherdumas <christopherdumas@me.com>
2015-08-13 08:11:05 -0700

    Added todo, and example/test adventure

---
+++ TODO.md
@@ -0,0 +1,5 @@
+TODO: IO is very slow.
+TODO: Add prompt.
+TODO: Make inventory actions less verbose.
+TODO: Automatically check object "get"s.
+TODO: Make an alternative room function that is more flexible. Call it custom-room or something.
---
+++ test.rkt
@@ -0,0 +1,27 @@
+#lang racket
+(require "./tral.rkt")
+(define inventory '())
+(define actions (add-inventory-actions (list
+					(list (list "inventory") (lambda (v)
+								   (lambda (state)
+								     (displayln inventory)
+								     state)))
+					(list (list "get" "lamp") (lambda (v n)
+								    (lambda (state)
+								      (displayln (cond
+										  [(equal? state "Entrance To Cave")
+										   (set! inventory (cons n inventory))
+										   "Taken."]
+										  [else "You can't see any such thing."]))
+								      state))))))
+(define room-entrance (room "Entrance To Cave" "You are in a fairly open field. To your north, the ground opens into a cave." '("lamp")))
+(define room-cave (room "Darkness" "It's to dark to see a thing." '()))
+
+(define main-fsm (make-fsm
+		  (list
+		   (list "Cave" "south" "Entrance To Cave" room-entrance)
+		   (list "Entrance To Cave" "north" "Cave" room-cave)
+		   (list "start" "begin" "Entrance To Cave" room-entrance))))
+
+(repl main-fsm "start" "begin" actions)
+
